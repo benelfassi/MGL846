00e4b77d3b9754debd10abfdee2fbdad5f6c79c2
abenelfassi <benelfassi@gmail.com>
2021-02-09 22:24:13 -0500

    new message

--- SystemeGestionPaie/database/payroll.sql
+++ SystemeGestionPaie/database/payroll.sql
@@ -1,25 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Apr 18, 2015 at 07:54 PM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
 -- Database: `payroll`
 --
+CREATE DATABASE payroll;
 
+use payroll;
 -- --------------------------------------------------------
 
 --
